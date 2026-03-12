6a4b2f90465195f70d7ce5cbbf08af2948e3841a
DVD <dmitrijnemov13@gmail.com>
2026-03-12 18:18:47 +0300

    Update turnip_builder_upstream.sh

--- turnip_builder_upstream.sh
+++ turnip_builder_upstream.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
-#Define variables
 green='\033[0;32m'
 red='\033[0;31m'
 nocolor='\033[0m'
+
 deps="git meson ninja patchelf unzip curl pip flex bison zip glslang glslangValidator"
 workdir="$(pwd)/turnip_workdir"
-magiskdir="$workdir/turnip_module"
 ndkver="android-ndk-r29"
 ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
 sdkver="34"
@@ -15,65 +14,60 @@ srcfolder="mesa"
 
 clear
 
-#There are 4 functions here, simply comment to disable.
-#You can insert your own function and make a pull request.
 run_all(){
-	echo "====== Begin building TU V$BUILD_VERSION! ======"
+	echo "====== Building Mesa Turnip A810 Only V$BUILD_VERSION ======"
 	check_deps
 	prepare_workdir
 	build_lib_for_android main tu8_kgsl.patch
 }
 
 check_deps(){
-	echo "Checking system for required Dependencies ..."
-		for deps_chk in $deps;
-			do
-				sleep 0.25
-				if command -v "$deps_chk" >/dev/null 2>&1 ; then
-					echo -e "$green - $deps_chk found $nocolor"
-				else
-					echo -e "$red - $deps_chk not found, can't countinue. $nocolor"
-					deps_missing=1
-				fi;
-			done
-
-		if [ "$deps_missing" == "1" ]
-			then echo "Please install missing dependencies" && exit 1
+	echo "Checking dependencies..."
+	for deps_chk in $deps; do
+		if command -v "$deps_chk" >/dev/null 2>&1 ; then
+			echo -e "$green - $deps_chk found $nocolor"
+		else
+			echo -e "$red - $deps_chk missing $nocolor"
+			deps_missing=1
 		fi
+	done
 
-	echo "Installing python Mako dependency (if missing) ..." $'\n'
-		pip install mako &> /dev/null
+	if [ "$deps_missing" == "1" ]; then
+		echo "Install missing dependencies and retry."
+		exit 1
+	fi
+
+	pip install mako &> /dev/null
 }
 
 prepare_workdir(){
-	echo "Preparing work directory ..." $'\n'
-		mkdir -p "$workdir" && cd "$_"
+	echo "Preparing work directory..."
+	mkdir -p "$workdir" && cd "$workdir"
 
-	echo "Downloading android-ndk from google server ..." $'\n'
-		curl https://dl.google.com/android/repository/"$ndkver"-linux.zip --output "$ndkver"-linux.zip &> /dev/null
-	echo "Exracting android-ndk ..." $'\n'
-		unzip "$ndkver"-linux.zip &> /dev/null
+	echo "Downloading Android NDK..."
+	curl -L https://dl.google.com/android/repository/"$ndkver"-linux.zip --output "$ndkver"-linux.zip &> /dev/null
+	unzip -q "$ndkver"-linux.zip &> /dev/null
 
-	echo "Downloading mesa source ..." $'\n'
-		git clone $mesasrc --depth=1 -b main $srcfolder
-		cd $srcfolder
-#	echo "Pushing TU_VERSION..."
-#		echo "#define TUGEN8_DRV_VERSION \"v$BUILD_VERSION\"" > ./src/freedreno/vulkan/tu_version.h
+	echo "Cloning Mesa upstream..."
+	git clone $mesasrc --depth=1 -b main $srcfolder
+	cd $srcfolder
+
+	echo "#define TUGEN8_DRV_VERSION \"v$BUILD_VERSION\"" > ./src/freedreno/vulkan/tu_version.h
 }
 
-
 build_lib_for_android(){
-	echo "==== Building Mesa on $1 branch ===="
-	#git reset --hard
-	echo "Applying patches... ($2)"
-    	wget https://github.com/whitebelyash/mesa-tu8/releases/download/patchset-head-v2/$2
-		if ! git apply --check $2; then
-			echo "Failed to apply $2!"
-			exit 1
-		fi
-    	git apply $2
-	#git checkout origin/$1
-	#Workaround for using Clang as c compiler instead of GCC
+	echo "==== Building Mesa on branch $1 ===="
+
+	echo "Downloading whitebelyash patchset..."
+	wget https://github.com/whitebelyash/mesa-tu8/releases/download/patchset-head-v2/$2
+
+	echo "Applying patchset..."
+	if ! git apply --check $2; then
+		echo "Patch failed!"
+		exit 1
+	fi
+	git apply $2
+
 	mkdir -p "$workdir/bin"
 	ln -sf "$ndk/clang" "$workdir/bin/cc"
 	ln -sf "$ndk/clang++" "$workdir/bin/c++"
@@ -86,14 +80,12 @@ build_lib_for_android(){
 	export OBJDUMP=llvm-objdump
 	export OBJCOPY=llvm-objcopy
 	export LDFLAGS="-fuse-ld=lld"
-	GITHASH=$(git rev-parse --short HEAD)
 
-	echo "Generating build files ..." $'\n'
-		cat <<EOF >"android-aarch64.txt"
+	cat <<EOF >"android-aarch64.txt"
 [binaries]
 ar = '$ndk/llvm-ar'
 c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang']
-cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments']
+cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++']
 c_ld = '$ndk/ld.lld'
 cpp_ld = '$ndk/ld.lld'
 strip = '$ndk/llvm-strip'
@@ -106,7 +98,7 @@ cpu = 'armv8'
 endian = 'little'
 EOF
 
-		cat <<EOF >"native.txt"
+	cat <<EOF >"native.txt"
 [build_machine]
 c = ['ccache', 'clang']
 cpp = ['ccache', 'clang++']
@@ -120,39 +112,40 @@ cpu = 'x86_64'
 endian = 'little'
 EOF
 
-		meson setup build-android-aarch64 \
-			--cross-file "android-aarch64.txt" \
-			--native-file "native.txt" \
-			--prefix /tmp/turnip-$1 \
-			-Dbuildtype=release \
-			-Dstrip=true \
-			-Dplatforms=android \
-			-Dvideo-codecs= \
-			-Dplatform-sdk-version="$sdkver" \
-			-Dandroid-stub=true \
-			-Dgallium-drivers= \
-			-Dvulkan-drivers=freedreno \
-			-Dvulkan-beta=true \
-			-Dfreedreno-kmds=kgsl \
-			-Degl=disabled \
-			-Dplatform-sdk-version=36 \
-			-Dandroid-libbacktrace=disabled \
-			--reconfigure
+	meson setup build-android-aarch64 \
+		--cross-file "android-aarch64.txt" \
+		--native-file "native.txt" \
+		--prefix /tmp/turnip-$1 \
+		-Dbuildtype=release \
+		-Db_lto=false \
+		-Dstrip=true \
+		-Dplatforms=android \
+		-Dvideo-codecs= \
+		-Dplatform-sdk-version="$sdkver" \
+		-Dandroid-stub=true \
+		-Dgallium-drivers= \
+		-Dvulkan-drivers=freedreno \
+		-Dvulkan-beta=true \
+		-Dfreedreno-kmds=kgsl \
+		-Degl=disabled \
+		-Dplatform-sdk-version=36 \
+		-Dandroid-libbacktrace=disabled \
+		--reconfigure
 
-	echo "Compiling build files ..." $'\n'
-		ninja -C build-android-aarch64 install
+	ninja -C build-android-aarch64 install
 
 	if ! [ -a /tmp/turnip-$1/lib/libvulkan_freedreno.so ]; then
-		echo -e "$red Build failed! $nocolor" && exit 1
+		echo -e "$red Build failed! $nocolor"
+		exit 1
 	fi
-	echo "Making the archive"
+
 	cd /tmp/turnip-$1/lib
 	cat <<EOF >"meta.json"
 {
   "schemaVersion": 1,
-  "name": "Mesa Turnip v$BUILD_VERSION-$GITHASH",
-  "description": "Mesa-git Freedreno/Turnip adapted for AdrenoTools (git $GITHASH)",
-  "author": "whitebelyash",
+  "name": "A810 Turnip Version-$BUILD_VERSION",
+  "description": "Upstream Mesa Turnip build (A810 Only, with whitebelyash patchset)",
+  "author": "DVD_Disk",
   "packageVersion": "1",
   "vendor": "Mesa",
   "driverVersion": "Vulkan 1.4.335",
@@ -160,11 +153,7 @@ EOF
   "libraryName": "libvulkan_freedreno.so"
 }
 EOF
-zip /tmp/mesa-turnip-$1-V$BUILD_VERSION.zip libvulkan_freedreno.so meta.json
-cd -
-if ! [ -a /tmp/mesa-turnip-$1-V$BUILD_VERSION.zip ]; then
-	echo -e "$red Failed to pack the archive! $nocolor"
-fi
-}
 
+	zip /tmp/a810_only-main-V$BUILD_VERSION.zip libvulkan_freedreno.so meta.json
+}
 run_all
