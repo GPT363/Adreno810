6e634d016004cf5558c9d4a0659f8659b8398774
DVD <disk_dvd@outlook.com>
2026-06-17 10:39:25 +0300

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -17,7 +17,7 @@ ndkver="android-ndk-r29"
 ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
 sdkver="34"
 mesasrc="https://github.com/DiskDVD/A8XX-Y"
-srcfolder="A8XX"
+srcfolder="Haks"
 driver_name="A8XX-Y"
 
 # Проверка наличия BUILD_VERSION
@@ -44,7 +44,7 @@ run_all(){
     echo ""
     check_deps
     prepare_workdir
-    build_lib_for_android A8XX
+    build_lib_for_android Haks
 }
 
 check_deps(){
